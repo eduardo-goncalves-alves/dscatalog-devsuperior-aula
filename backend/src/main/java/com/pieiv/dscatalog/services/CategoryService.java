a7d293306296f3085cab9bc256e824289f5965b2
Eduardo Gonçalves <eduardo.ga@estudante.iftm.edu.br>
2025-12-16 16:24:54 -0300

    Database seeding

--- backend/src/main/java/com/pieiv/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/pieiv/dscatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.pieiv.dscatalog.services;
 
 import com.pieiv.dscatalog.entities.Category;
 import com.pieiv.dscatalog.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional
     public List<Category> findAll() {
         return repository.findAll();
     }
